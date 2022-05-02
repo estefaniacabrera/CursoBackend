dff4b7c154c2eefbece5cdda2e4f514f0d90d734
Estefania Cabrera <estefaniacabrera13@gmail.com>
2022-05-01 21:52:05 -0300

    entrega corregida

--- servidor.js
+++ servidor.js
@@ -10,9 +10,13 @@ const archivo = new Contenedor('./productos.txt')
 const server = app.listen(PORT, () => {
     console.log('Servidor HTTP escuchando en el puerto ' + PORT)
 })
-app.get('/productos', (req, res) => { 
-    res.send('Productos: ' + archivo.getAll().then(val => (console.log({ val }))))
+app.get('/productos',async (req, res) => { 
+    let productos = await archivo.getAll()
+    console.log(productos.length);
+    res.json(productos)
 })
-app.get('/productoRandom', (req, res) => { 
-    //res.send(Math.floor(Math.random(archivo.getAll()) * productos.lenght))
+app.get('/productoRandom', async (req, res) => { 
+    let productosTodos = await archivo.getAll()
+    let numero = Math.floor(Math.random() * productosTodos.length)
+    res.json(await archivo.getById(numero))
 })
